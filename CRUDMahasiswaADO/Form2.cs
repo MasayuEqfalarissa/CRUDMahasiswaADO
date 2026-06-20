23e86f5308b39111b62cd88d6dc3d4ec77972c36
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2026-06-21 00:36:17 +0700

    Membuat desain filter laporan rekap data pada Form2

---
+++ CRUDMahasiswaADO/Form2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form2 : Form
+    {
+        // Connection string disamakan dengan Form1
+        static string connectionString = "Data Source=MASAYUU-DN5OL92\\MASAYU;Initial Catalog=DBAkademikADO;Integrated Security=True";
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+
+        DAL dbLogic = new DAL();
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            dtpTanggalMasuk.Format = DateTimePickerFormat.Custom;
+            dtpTanggalMasuk.CustomFormat = "yyyy";
+            dtpTanggalMasuk.ShowUpDown = true;
+            dtpTanggalMasuk.MinDate = new DateTime(2000, 1, 1);
+            dtpTanggalMasuk.MaxDate = DateTime.Now;
+
+            cmbProdi.DropDownStyle = ComboBoxStyle.DropDownList;
+            btnCetak.Enabled = false;
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+
+                SqlCommand cmd = new SqlCommand("select namaprodi from programstudi", conn);
+                cmd.CommandType = CommandType.Text;
+                dtProdi = new DataTable();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(dtProdi);
+                cmbProdi.DataSource = dtProdi;
+                cmbProdi.DisplayMember = "namaprodi";
+                cmbProdi.ValueMember = "namaprodi";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal Load Data: " + ex.Message);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@inProdi", SqlDbType.VarChar, 50).Value = cmbProdi.SelectedValue;
+                cmd.Parameters.Add("@inTglMsuk", SqlDbType.VarChar, 4).Value = dtpTanggalMasuk.Value.Year.ToString();
+
+                da = new SqlDataAdapter(cmd);
+                dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+
+                dataGridView1.DataSource = dtMahasiswa;
+
+                if (dtMahasiswa.Rows.Count > 0)
+                {
+                    btnCetak.Enabled = true;
+                }
+                else
+                {
+                    btnCetak.Enabled = false;
+                    MessageBox.Show("Data tidak ditemukan");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal Load Data: " + ex.Message);
+            }
+        }
+
+        private void btnCetak_Click(object sender, EventArgs e)
+        {
+            // Buka Form3 (Report) dan kirim parameter filter ke sana
+            Form3 frm3 = new Form3(cmbProdi.SelectedValue.ToString(), dtpTanggalMasuk.Value);
+            frm3.Show();
+            this.Hide();
+        }
+
+        // BIARKAN KOSONG: Agar komponen Designer tidak error
+        private void lblJudul_Click(object sender, EventArgs e) { }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void dtpTanggalMasuk_ValueChanged(object sender, EventArgs e) { }
+    }
+}
